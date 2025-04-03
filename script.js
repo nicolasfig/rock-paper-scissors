d08b92360ad836df6e37f117a17d30db4e9bb73c
Nicolas <nicolasfigb@gmail.com>
2025-04-02 23:37:46 -0500

    Initial setup

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+console.log(getComputerChoice(1, 3));
